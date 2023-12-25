dd6a70b0abdc5d23c0d0108874bac38907c69e45
Hiten324812 <hitenmistry354@gmail.com>
2023-12-26 01:09:04 +0530

    body parser commit

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 
-app.use( (req,res,next) => {
-    console.log('in the middleware !');
+app.use(bodyParser.urlencoded( {extended: true}));
 
+app.use( '/add-product',(req,res,next) => {
+  
+   res.send('<form action="/product" method="POST"> <input type="text" name="message"> <button type="submit"> send data </button> </form>');
+    // ..
+});
+
+app.use('/product',(req,res,next) =>{
+    console.log(req.body);
+    res.redirect('/');
+    });
+
+app.use('/',(req,res,next) => {
     res.send("<h1> hello hi my name is express !!! </h1>");
     // ..
 });
