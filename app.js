6c85022a64573e162bae93986253e15b92c51049
Hiten324812 <hitenmistry354@gmail.com>
2023-12-28 23:59:11 +0530

    pug and handlebars completed

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expresshandle = require('express-handlebars');
 
 const app = express();
 
-const adminroutes = require('./routes/admin');
+app.engine('hbs',expresshandle());
+app.set('view engine','hbs');
+app.set('views','views');
+
+const admindata = require('./routes/admin');
 const shoproutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded( {extended: true}));
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/admin',adminroutes);
-app.use(shoproutes);
+app.use('/admin',admindata.routes); 
+app.use(shoproutes.routes);
 
 app.use((req,res) => {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const express = require('express');
 const rootdir = require('../util/path');
+const admindata = require('../routes/admin');
 
 
 const router123 = express.Router();
 
 router123.get('/',(req,res,next) => {
-
-   res.sendFile(path.join(rootdir ,'views','shop.html'));
+    const products = admindata.products;
+    res.render('shop',{prods : products , pageTitle : 'shop flipkart' , path : '/' , hasproduct : products.length > 0});
     // ..
 });
 
-module.exports = router123;
+module.exports.routes = router123;
